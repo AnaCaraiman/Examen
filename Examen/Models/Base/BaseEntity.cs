54016561e03558b1659c4a180f98f7ea8b603f5b
AnaCaraiman <anacaraiman51@gmail.com>
2024-01-13 21:48:27 +0200

    fix BaseEntity

--- Examen/Models/Base/BaseEntity.cs
+++ Examen/Models/Base/BaseEntity.cs
@@ -1,7 +1,11 @@
-﻿namespace Examen.Models.Base
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Examen.Models.Base
 {
     public class BaseEntity : IBaseEntity
     {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public Guid Id { get; set; }
         public DateTime CreatedAt { get; set; } = DateTime.Now;
         public DateTime UpdatedAt { get; set; } = DateTime.Now;
